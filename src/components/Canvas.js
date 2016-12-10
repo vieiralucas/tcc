e7b30a67c6dadcb0df224f766c49456dff723538
Lucas Vieira <vieiralucas4@gmail.com>
2016-12-10 16:09:02 -0200

    Implement zoom

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -1,10 +1,44 @@
 // @flow
 
-import React, { Component } from 'react';
+import React, { Component, Children } from 'react';
+
+type WheelEvent = {
+  deltaY: number
+};
 
 class Canvas extends Component {
+  state: {
+    scale: number;
+  };
+
+  constructor() {
+    super();
+
+    this.state = {
+      scale: 1
+    };
+  };
+
+  handleZoom({ deltaY }: WheelEvent) {
+    const newScale = this.state.scale + deltaY * 0.1;
+    this.setState({
+      scale: newScale >= 1 ? newScale : 1
+    });
+  }
+
   render() {
-    return <svg>{this.props.children}</svg>;
+    const { scale } = this.state;
+    const { children, width, height, style } = this.props;
+
+    const childrenWithScale = Children.map(children,
+                                           child => React.cloneElement(child, { scale }));
+
+    return (
+      <svg width={ width } height={ height } style={ style }
+        onWheel={ this.handleZoom.bind(this) }>
+        { childrenWithScale }
+      </svg>
+    );
   }
 }
 
