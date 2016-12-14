98550036dcf40d2d7c3445527c3148098cc31184
Lucas Vieira <vieiralucas4@gmail.com>
2016-12-14 21:30:36 -0200

    Remove svg from UseCase component

--- src/components/uml/UseCase.js
+++ src/components/uml/UseCase.js
@@ -5,31 +5,29 @@ import BaseUML from './BaseUML';
 import type { UseCaseProps } from '../../types';
 
 const UseCase = (props: UseCaseProps) => {
-  const { name, x, y } = props;
-  const fontSize = 20;
+  const { name } = props;
+  const fontSize = 10;
   const width = 200;
   const lines = name.match(/.{1,30}/g) || [];
   const height = fontSize * 3 * lines.length;
 
-  const nameStyle = {
+  const style = {
     fontSize: `${fontSize}px`,
+    border: 'solid',
+    borderWidth: '1px',
+    borderRadius: '100%',
+    cursor: 'pointer',
+    textAlign: 'center',
+    whiteSpace: 'pre-wrap',
+    wordWrap: 'break-word',
+    width
   };
-
-  const tspans = lines.map((line, i) => <tspan x={'50%'} dy={'1.3em'} key={i}>{line}</tspan>);
-  const useCaseSvg = (
-    <svg x={x} y={y} width={width} height={height} cursor={'pointer'}>
-      <ellipse cx={width / 2} cy={height / 2}
-        rx={width / 2} ry={height / 2}
-        fillOpacity={0} stroke="black" strokeWidth={1} />
-      <text textAnchor={'middle'} style={nameStyle}>
-        {tspans}
-      </text>
-    </svg>
-  );
 
   return (
     <BaseUML { ...props } width={width} height={height}>
-      { useCaseSvg }
+      <div style={style}>
+        <p>{ name }</p>
+      </div>
     </BaseUML>
   );
 };
