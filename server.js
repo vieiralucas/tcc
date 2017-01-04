7b6830c47ee250d451c64278e06e2e3167c12700
Lucas Vieira <vieiralucas4@gmail.com>
2017-01-04 00:19:43 -0200

    Add sequelize + postgres + create user model

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 
+const sequelize = require('./lib/models/sequelize');
+const User = require('./lib/models/user');
+
 const app = express();
 
 app.set('port', (process.env.PORT || 3001));
@@ -8,6 +11,9 @@ if (process.env.NODE_ENV === 'production') {
 	app.use(express.static('client/build'));
 }
 
-app.listen(app.get('port'), () => {
-	console.log(`Express server running at ${app.get('port')}`);
+sequelize.sync().then(() => {
+  app.listen(app.get('port'), () => {
+    console.log(`Express server running at ${app.get('port')}`);
+  });
 });
+
