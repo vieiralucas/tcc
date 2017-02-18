e15b958cd20a67b0e3204e2b906442a300a8d5df
Lucas Vieira <vieiralucas4@gmail.com>
2017-02-18 02:56:29 -0200

    Add create usecase api

---
+++ acceptance/usecases.acceptance.js
@@ -0,0 +1,81 @@
+const { expect } = require('chai');
+
+const { app, token } = require('./support');
+const { Usecase, Project } = require('../lib/models');
+
+const api = app.api;
+
+describe('UseCases API', () => {
+  describe('POST /api/usecases', () => {
+    let project;
+
+    beforeEach(() => Project.findOne({})
+      .then(_project => {
+        project = JSON.parse(JSON.stringify(_project));
+      })
+    );
+
+    it('should need authentication', () => {
+      return api.post('/api/usecases')
+        .set('Content-Type', 'application/json')
+        .send(JSON.stringify({ name: 'usecase', description: 'usecase description' }))
+        .expect(401);
+    });
+
+    it('should create a new usecase', () => {
+      let body;
+
+      return api.post('/api/usecases')
+        .set('Content-Type', 'application/json')
+        .set('Authorization', `Bearer ${token}`)
+        .send(JSON.stringify({
+          name: 'usecase',
+          description: 'usecase description',
+          project: project._id
+        }))
+        .expect(201)
+        .expect(({ body: _body }) => {
+          expect(_body).to.have.property('name', 'usecase');
+          expect(_body).to.have.property('description', 'usecase description');
+          expect(_body).to.have.property('_id').which.is.a('string');
+          expect(_body).to.have.property('project', project._id);
+
+          body = _body;
+        })
+        .then(() => Usecase.findById(body._id)
+          .then(usecase => {
+            expect(JSON.parse(JSON.stringify(usecase))).to.deep.equal(body);
+          })
+        );
+    });
+
+    it('should send 400 if invalid data', () => api.post('/api/usecases')
+      .set('Content-Type', 'application/json')
+      .set('Authorization', `Bearer ${token}`)
+      .send(JSON.stringify({
+        description: 'usecase description',
+        project: project._id
+      }))
+      .expect(400)
+      .expect(({ body }) => {
+        expect(body).to.have.property('message', 'Usecase validation failed');
+      })
+    );
+
+    it('should send 400 if projectId does not exists', () => {
+      const id = '58a4fc3be818ee667ff53e3d';
+      return api.post('/api/usecases')
+        .set('Content-Type', 'application/json')
+        .set('Authorization', `Bearer ${token}`)
+        .send(JSON.stringify({
+          name: 'usecase',
+          description: 'usecase description',
+          project: id
+        }))
+        .expect(400)
+        .expect(({ body }) => {
+          expect(body).to.have.property('message', `Project ${id} does not exist`);
+        });
+    });
+  });
+});
